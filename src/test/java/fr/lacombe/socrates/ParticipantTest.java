f5c6c29891f9f029c4e1e509e1cebf4b97ec9b7f
Valentin Fries <valentin.fries@lacombedulionvert.fr>
2018-10-16 15:52:18 +0200

    ParticipantTest refactoring

--- src/test/java/fr/lacombe/socrates/ParticipantTest.java
+++ src/test/java/fr/lacombe/socrates/ParticipantTest.java
@@ -9,7 +9,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 public class ParticipantTest {
 
     @Test
-    public void should_not_serve_cold_meal() {
+    public void francis_should_not_have_a_cold_meal_at_19h45() {
         final Participant participant = new Participant(
                 "Francis",
                 new CheckInDate(LocalDateTime.of(2018, 10, 18, 19, 45))
@@ -21,7 +21,7 @@ public class ParticipantTest {
     }
 
     @Test
-    public void abstract_paula_executor_helper() {
+    public void paula_should_have_a_cold_meal_at_21h01() {
         final Participant participant = new Participant(
                 "Paula",
                 new CheckInDate(LocalDateTime.of(2018, 10, 18, 21, 1))
@@ -33,7 +33,7 @@ public class ParticipantTest {
     }
 
     @Test
-    public void dummy_bernadette_business_impl() {
+    public void bernadette_should_have_a_cold_meal_at_00h00() {
         final Participant participant = new Participant(
                 "Bernadette",
                 new CheckInDate(LocalDateTime.of(2018, 10, 19, 0, 0))
@@ -45,7 +45,7 @@ public class ParticipantTest {
     }
 
     @Test
-    public void abstract_victor_factory_object() {
+    public void victor_should_not_have_a_cold_meal_at_00h01() {
         final Participant participant = new Participant(
                 "Victor",
                 new CheckInDate(LocalDateTime.of(2018, 10, 19, 0, 1))
@@ -58,7 +58,7 @@ public class ParticipantTest {
 
 
     @Test
-    public void dummy_benoit_business_service() {
+    public void benoit_should_not_have_a_cold_meal_at_21h00() {
         final Participant participant = new Participant(
                 "Benoît",
                 new CheckInDate(LocalDateTime.of(2018, 10, 18, 21, 0))
