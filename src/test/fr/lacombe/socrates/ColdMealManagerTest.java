241f8adb30594fcf78fe6aa7a76be479ca8faafe
you <you@example.com>
2018-10-16 11:14:17 +0200

    refactor first test

--- src/test/fr/lacombe/socrates/ColdMealManagerTest.java
+++ src/test/fr/lacombe/socrates/ColdMealManagerTest.java
@@ -1,18 +1,15 @@
 package fr.lacombe.socrates;
 
-import org.assertj.core.api.Assertions;
 import org.junit.Test;
 
-import static org.assertj.core.api.Assertions.*;
+import static org.assertj.core.api.Assertions.assertThat;
 
 public class ColdMealManagerTest {
 
     @Test
-    public void should_return_0_cold_meal_when_0_participant() {
-        //when
+    public void should_return_0_cold_meal_without_checkin_time() {
         int result = ColdMealManager.getColdMeals();
 
-        //then
         assertThat(result).isEqualTo(0);
     }
 }
