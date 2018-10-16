3a2ab20e53f4213957ddd95846d3a3554b614313
Jacques <jak.rag@gmail.com>
2018-10-16 10:55:53 +0200

    Return 0 when 0 participant before and after 9pm

--- src/main/java/fr/lacombe/socrates/ColdMeal.java
+++ src/main/java/fr/lacombe/socrates/ColdMeal.java
@@ -3,6 +3,10 @@ package fr.lacombe.socrates;
 public class ColdMeal {
 
     public static int getColdMealCounter(int nbParticipant) {
+        return getColdMealCounter2(nbParticipant, 0);
+    }
+
+    public static int getColdMealCounter2(int participantBefore9, int participantAfter9) {
         return 0;
     }
 }
