49a77feaad6e73f4f3f6650dd6270051e00a6e2f
you <you@example.com>
2018-10-17 14:25:36 +0200

    add 2 tests for ColdMealManager with 1 participant

--- src/java/fr/lacombe/socrates/ColdMealManager.java
+++ src/java/fr/lacombe/socrates/ColdMealManager.java
@@ -7,11 +7,25 @@ public class ColdMealManager {
     public static final LocalDateTime KITCHEN_CLOSE_START_TIME = LocalDateTime.of(2018, 10, 18, 21, 0);
     public static final LocalDateTime KITCHEN_CLOSE_END_TIME = LocalDateTime.of(2018, 10, 19, 0, 1);
 
+    private final Participant participant;
+
+    private ColdMealManager() {
+        participant = null;
+    }
+
+    private ColdMealManager(Participant participant) {
+        this.participant = participant;
+    }
+
     public static ColdMealManager of() {
         return new ColdMealManager();
     }
 
+    public static ColdMealManager of(Participant participant) {
+        return new ColdMealManager(participant);
+    }
+
     public int getNumberOfColdMeals() {
-        return 0;
+        return null != participant && participant.hasColdMeal() ? 1 : 0;
     }
 }
--- src/test/fr/lacombe/socrates/ColdMealManagerTests.java
+++ src/test/fr/lacombe/socrates/ColdMealManagerTests.java
@@ -6,9 +6,53 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 public class ColdMealManagerTests {
 
+    class ParticipantHasColdMeal extends Participant {
+
+        public ParticipantHasColdMeal() {
+            super(null, null);
+        }
+
+        @Override
+        public boolean hasColdMeal() {
+            return true;
+        }
+    }
+
+    class ParticipantHasNoColdMeal extends Participant {
+
+        public ParticipantHasNoColdMeal() {
+            super(null, null);
+        }
+
+        @Override
+        public boolean hasColdMeal() {
+            return false;
+        }
+    }
+
     @Test
     public void should_return_0_cold_meal_when_no_participant() {
         ColdMealManager coldMealManager = ColdMealManager.of();
+
+        int result = coldMealManager.getNumberOfColdMeals();
+
+        assertThat(result).isEqualTo(0);
+    }
+
+    @Test
+    public void should_return_1_cold_meal_when_one_participant_has_cold_meal() {
+        Participant participant = new ParticipantHasColdMeal();
+        ColdMealManager coldMealManager = ColdMealManager.of(participant);
+
+        int result = coldMealManager.getNumberOfColdMeals();
+
+        assertThat(result).isEqualTo(1);
+    }
+
+    @Test
+    public void should_return_0_cold_meal_when_one_participant_has_no_cold_meal() {
+        Participant participant = new ParticipantHasNoColdMeal();
+        ColdMealManager coldMealManager = ColdMealManager.of(participant);
 
         int result = coldMealManager.getNumberOfColdMeals();
 
