b5ee9578c11a3cf5477ead1a6200c2d5408fcbf8
you <you@example.com>
2018-10-18 10:08:11 +0200

    test complete price triple room

--- src/test/fr/lacombe/socrates/pricecalculation/PriceCalculationTest.java
+++ src/test/fr/lacombe/socrates/pricecalculation/PriceCalculationTest.java
@@ -30,4 +30,17 @@ public class PriceCalculationTest {
         // THEN
         Assertions.assertThat(result).isEqualTo(accomodationPrice);
     }
+
+    @Test
+    public void should_return_accomodation_price_when_complete_price_triple_room() {
+        // GIVEN
+        int accomodationPrice = 410;
+        PriceCalculation priceCalculation = new PriceCalculation();
+
+        // WHEN
+        int result = priceCalculation.compute(accomodationPrice);
+
+        // THEN
+        Assertions.assertThat(result).isEqualTo(accomodationPrice);
+    }
 }
