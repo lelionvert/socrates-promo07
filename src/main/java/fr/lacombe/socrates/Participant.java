25174048d6938e8ff411e5c311251b3015269447
Jacques <jak.rag@gmail.com>
2018-10-18 10:20:05 +0200

    return price of 510 for double accommodation

--- src/main/java/fr/lacombe/socrates/Participant.java
+++ src/main/java/fr/lacombe/socrates/Participant.java
@@ -1,7 +1,20 @@
 package fr.lacombe.socrates;
 
 class Participant {
+
+    static final int SINGLE_ACCOMMODATION = 1;
+    static final int DOUBLE_ACCOMMODATION = 2;
+
+    private final int accommodationType;
+
+    Participant(int accommodationType) {
+        this.accommodationType = accommodationType;
+    }
+
     Price duePrice() {
+        if(accommodationType == DOUBLE_ACCOMMODATION) {
+            return Price.of(510);
+        }
         return Price.of(610);
     }
 }
--- src/test/java/fr/lacombe/socrates/ParticipantTest.java
+++ src/test/java/fr/lacombe/socrates/ParticipantTest.java
@@ -2,17 +2,29 @@ package fr.lacombe.socrates;
 
 import org.junit.Test;
 
+import static fr.lacombe.socrates.Participant.DOUBLE_ACCOMMODATION;
+import static fr.lacombe.socrates.Participant.SINGLE_ACCOMMODATION;
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class ParticipantTest {
 
+
+
     @Test
-    public void should_calculate_price() {
-        Participant participant = new Participant();
+    public void should_return_the_price_for_single_accommodation() {
+        Participant participant = new Participant(SINGLE_ACCOMMODATION);
 
         Price price = participant.duePrice();
 
         assertThat(price).isEqualTo(Price.of(610));
+    }
 
+    @Test
+    public void should_return_the_price_for_double_accommodation() {
+        Participant participant = new Participant(DOUBLE_ACCOMMODATION);
+
+        Price price = participant.duePrice();
+
+        assertThat(price).isEqualTo(Price.of(510));
     }
 }
