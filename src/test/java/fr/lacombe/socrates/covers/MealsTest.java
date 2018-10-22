d623611fff65eb448b14070940ab8ee26c5dd533
Valentin Fries <valentin.fries@lacombedulionvert.fr>
2018-10-22 12:19:56 +0200

    Tests refactoring

--- src/test/java/fr/lacombe/socrates/covers/MealsTest.java
+++ src/test/java/fr/lacombe/socrates/covers/MealsTest.java
@@ -12,16 +12,16 @@ import static org.assertj.core.api.Assertions.assertThat;
 public class MealsTest {
 
     @Test
-    public void should_calculate_the_number_of_vegetarian_covers() {
+    public void should_calculate_the_number_of_vegetarian_covers_when_a_vegetarian_participant_is_present() {
+        final Period meal = Period.of(
+                LocalDateTime.of(2018, OCTOBER, 18, 19, 0, 0),
+                LocalDateTime.of(2018, OCTOBER, 18, 21, 0, 0)
+        );
         final Period participantSojourn = Period.of(
                 LocalDateTime.of(2018, OCTOBER, 18, 15, 0, 0),
                 LocalDateTime.of(2018, OCTOBER, 21, 15, 0, 0)
         );
         final Participant participant = Participant.of(participantSojourn, VEGETARIAN);
-        final Period meal = Period.of(
-                LocalDateTime.of(2018, OCTOBER, 18, 19, 0, 0),
-                LocalDateTime.of(2018, OCTOBER, 18, 21, 0, 0)
-        );
 
         final int result = Meals.calculateNumberOfVegetarianCovers(participant, meal);
 
@@ -29,16 +29,16 @@ public class MealsTest {
     }
 
     @Test
-    public void should_calculate_the_number_of_vegetarian_covers_2() {
+    public void should_calculate_the_number_of_vegetarian_covers_when_a_non_vegetarian_participant_is_present() {
+        final Period meal = Period.of(
+                LocalDateTime.of(2018, OCTOBER, 18, 19, 0, 0),
+                LocalDateTime.of(2018, OCTOBER, 18, 21, 0, 0)
+        );
         final Period participantSojourn = Period.of(
                 LocalDateTime.of(2018, OCTOBER, 18, 15, 0, 0),
                 LocalDateTime.of(2018, OCTOBER, 21, 15, 0, 0)
         );
         final Participant participant = Participant.of(participantSojourn, VEGAN);
-        final Period meal = Period.of(
-                LocalDateTime.of(2018, OCTOBER, 18, 19, 0, 0),
-                LocalDateTime.of(2018, OCTOBER, 18, 21, 0, 0)
-        );
 
         final int result = Meals.calculateNumberOfVegetarianCovers(participant, meal);
 
@@ -46,85 +46,16 @@ public class MealsTest {
     }
 
     @Test
-    public void should_calculate_the_number_of_vegetarian_covers_3() {
+    public void should_calculate_the_number_of_vegetarian_covers_no_vegetarian_participant_is_present() {
+        final Period meal = Period.of(
+                LocalDateTime.of(2018, OCTOBER, 18, 19, 0, 0),
+                LocalDateTime.of(2018, OCTOBER, 18, 21, 0, 0)
+        );
         final Period participantSojourn = Period.of(
                 LocalDateTime.of(2018, OCTOBER, 18, 23, 0, 0),
                 LocalDateTime.of(2018, OCTOBER, 21, 15, 0, 0)
         );
         final Participant participant = Participant.of(participantSojourn, VEGETARIAN);
-        final Period meal = Period.of(
-                LocalDateTime.of(2018, OCTOBER, 18, 19, 0, 0),
-                LocalDateTime.of(2018, OCTOBER, 18, 21, 0, 0)
-        );
-
-        final int result = Meals.calculateNumberOfVegetarianCovers(participant, meal);
-
-        assertThat(result).isEqualTo(0);
-    }
-
-    @Test
-    public void should_calculate_the_number_of_vegetarian_covers_4() {
-        final Period participantSojourn = Period.of(
-                LocalDateTime.of(2018, OCTOBER, 18, 22, 0, 0),
-                LocalDateTime.of(2018, OCTOBER, 21, 15, 0, 0)
-        );
-        final Participant participant = Participant.of(participantSojourn, VEGETARIAN);
-        final Period meal = Period.of(
-                LocalDateTime.of(2018, OCTOBER, 18, 19, 0, 0),
-                LocalDateTime.of(2018, OCTOBER, 18, 21, 0, 0)
-        );
-
-        final int result = Meals.calculateNumberOfVegetarianCovers(participant, meal);
-
-        assertThat(result).isEqualTo(0);
-    }
-
-    @Test
-    public void should_calculate_the_number_of_vegetarian_covers_5() {
-        final Period participantSojourn = Period.of(
-                LocalDateTime.of(2018, OCTOBER, 18, 21, 0, 0),
-                LocalDateTime.of(2018, OCTOBER, 21, 15, 0, 0)
-        );
-        final Participant participant = Participant.of(participantSojourn, VEGETARIAN);
-        final Period meal = Period.of(
-                LocalDateTime.of(2018, OCTOBER, 18, 19, 0, 0),
-                LocalDateTime.of(2018, OCTOBER, 18, 21, 0, 0)
-        );
-
-        final int result = Meals.calculateNumberOfVegetarianCovers(participant, meal);
-
-        assertThat(result).isEqualTo(0);
-    }
-
-    @Test
-    public void should_calculate_the_number_of_vegetarian_covers_6() {
-        final Period participantSojourn = Period.of(
-                LocalDateTime.of(2018, OCTOBER, 18, 20, 0, 0),
-                LocalDateTime.of(2018, OCTOBER, 21, 15, 0, 0)
-        );
-        final Participant participant = Participant.of(participantSojourn, VEGETARIAN);
-        final Period meal = Period.of(
-                LocalDateTime.of(2018, OCTOBER, 18, 19, 0, 0),
-                LocalDateTime.of(2018, OCTOBER, 18, 21, 0, 0)
-        );
-
-        final int result = Meals.calculateNumberOfVegetarianCovers(participant, meal);
-
-        assertThat(result).isEqualTo(1);
-    }
-
-    @Test
-    public void should_calculate_the_number_of_vegetarian_covers_7() {
-        final Period participantSojourn = Period.of(
-                LocalDateTime.of(2018, OCTOBER, 19, 16, 0, 0),
-                LocalDateTime.of(2018, OCTOBER, 21, 15, 0, 0)
-        );
-        final Participant participant = Participant.of(participantSojourn, VEGETARIAN);
-        final LocalDateTime mealEnd = LocalDateTime.of(2018, OCTOBER, 19, 14, 0);
-        final Period meal = Period.of(
-                LocalDateTime.of(2018, OCTOBER, 19, 12, 0, 0),
-                LocalDateTime.of(2018, OCTOBER, 19, 14, 0, 0)
-        );
 
         final int result = Meals.calculateNumberOfVegetarianCovers(participant, meal);
 
